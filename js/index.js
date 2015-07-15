2f89a4346d0bd3645b8f4e69b287310dd0cfd17e
nstepheyim <e045861@c02mt0kcf6t6.corp.mastercard.org>
2015-07-15 16:54:18 -0400

    added docs

---
+++ js/index.js
@@ -0,0 +1,15 @@
+// HINT:
+//
+// When you press Enter to submit a new cash register entry, the form will 
+// actually submit data and reload the page. You don't want to reload the page. It 
+// messes up the running tab. So in order to prevent normal form submission, 
+// make sure your form submit handler looks like this:
+//
+	// $('#entry').submit(function(e) {
+	// 		e.preventDefault();
+	//      .... do other stuff here ....
+	// 
+	// });
+
+
+
