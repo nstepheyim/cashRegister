9cce2f6aa851a3f94edca70a7afd4f5cf4700985
nstepheyim <e045861@C02MT0KCF6T6.local>
2015-07-16 18:28:42 -0400

    fixed <tr> problem, had to add <th>

--- js/index.js
+++ js/index.js
@@ -1,15 +1,36 @@
-// HINT:
-//
-// When you press Enter to submit a new cash register entry, the form will 
-// actually submit data and reload the page. You don't want to reload the page. It 
-// messes up the running tab. So in order to prevent normal form submission, 
-// make sure your form submit handler looks like this:
-//
-	// $('#entry').submit(function(e) {
-	// 		e.preventDefault();
-	//      .... do other stuff here ....
-	// 
-	// });
+//Stephanie Yim
+//CashRegister HW for GA FEWD61 2015 
+//July 17th, 2015
+
+
+$(function () {
+
+	var total = 0.0;
+
+ 	function addLine () {
+		// get and store user input
+		var newAmount = $('#newEntry').val(); 
+		newAmount = parseFloat(newAmount);
+		//calculate new total
+		total = total + newAmount;
+		$('#entries').append('<tr><td></td><td>$'+ newAmount.toFixed(2) + '</td></tr>');
+		
+
+		$('#total').html(total);
+	}
+
+
+	$('#entry').submit(function(e) {
+		e.preventDefault();
+		
+		addLine();
+
+		//clear input field
+		$('#newEntry').val(''); 
+	});
+
+});
+
 
 
 
